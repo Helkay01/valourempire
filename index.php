8871a7de6b5d4108255bcc1ead47c66d88a63e08
Helkay01 <83680190+Helkay01@users.noreply.github.com>
2025-08-28 19:11:36 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ include 'connections.php';
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Responsive Dashboard</title>
+  <title>Dashboard</title>
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-100">
