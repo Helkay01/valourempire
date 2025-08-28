b662c396198f5fd9591fccef14861ec840149486
Helkay01 <83680190+Helkay01@users.noreply.github.com>
2025-08-28 19:37:49 +0100

    Update receipts.php

--- receipts.php
+++ receipts.php
@@ -100,8 +100,8 @@ foreach ($dets as $det) {
         </div>
 
         <div>
-          <label for="clientEmail" class="block text-sm font-medium text-gray-700">Client Email (optional)</label>
-          <input type="email" name="clientEmail" id="clientEmail" class="mt-1 w-full border px-4 py-2 rounded focus:ring-2 focus:ring-blue-500"/>
+          <label for="clientEmail" class="block text-sm font-medium text-gray-700">Phone no</label>
+          <input type="text" name="clientEmail" id="clientEmail" required class="mt-1 w-full border px-4 py-2 rounded focus:ring-2 focus:ring-blue-500"/>
         </div>
 
         <div>
