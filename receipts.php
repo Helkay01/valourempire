44631b53f3ef3ee2a90f5a173a742ecf9696275e
Helkay01 <83680190+Helkay01@users.noreply.github.com>
2025-08-28 19:35:10 +0100

    Update receipts.php

--- receipts.php
+++ receipts.php
@@ -40,8 +40,8 @@ $res = $pdo->query($sel);
 $dets = $res->fetchAll(PDO::FETCH_ASSOC);
 $clientMap = [];
 foreach ($dets as $det) {
-   // $clientMap[$det['name']] = $det['id'];
-    $clientMap[$det['unpaid']] = $det['id'];
+    $clientMap[$det['name']] = $det['id'];
+
 }
 ?>
 
@@ -91,7 +91,7 @@ foreach ($dets as $det) {
         <div>
           <label for="searchClient" class="block text-sm font-medium text-gray-700">Client Name</label>
           <input list="clientName" required id="searchClient" placeholder="Select or type client name..." class="px-4 py-2 border border-gray-300 rounded-md w-full focus:ring-2 focus:ring-blue-500 outline-none" />
-          <input type="hidde" id="clientId" name="clientId" />
+          <input type="hidden" id="clientId" name="clientId" />
           <datalist id="clientName">
             <?php foreach ($dets as $det): ?>
               <option value="<?= htmlspecialchars($det['name']) ?>"></option>
