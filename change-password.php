a622d403009a9e4e4ea4777851ac8db236029417
Helkay01 <83680190+Helkay01@users.noreply.github.com>
2025-08-29 23:53:51 +0100

    Update change-password.php

--- change-password.php
+++ change-password.php
@@ -4,7 +4,7 @@ require 'connections.php'; // Assumes $conn is a PDO instance
 
 // Ensure user is logged in
 if (!isset($_SESSION['user'])) {
-    die("Unauthorized access.");
+   // die("Unauthorized access.");
     header("Location: login.php");
 }
 
