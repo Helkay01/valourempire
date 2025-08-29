0a7282d2e7671f8ac1db228c2a47301c63bf2a6a
Helkay01 <83680190+Helkay01@users.noreply.github.com>
2025-08-29 22:33:50 +0100

    Update receipts.php

--- receipts.php
+++ receipts.php
@@ -190,8 +190,6 @@ foreach ($dets as $det) {
           <select name="paymentMethod" id="paymentMethod" class="mt-1 w-full border px-4 py-2 rounded focus:ring-2 focus:ring-blue-500">
             <option>Cash</option>
             <option>Bank Transfer</option>
-            <option>Credit Card</option>
-            <option>Mobile Payment</option>
           </select>
         </div>
       </div>
