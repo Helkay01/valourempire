156dabf6e85161230c6d1cabc8789835a94c860f
Helkay01 <83680190+Helkay01@users.noreply.github.com>
2025-08-30 00:05:17 +0100

    Update edit-invoice.php

--- edit-invoice.php
+++ edit-invoice.php
@@ -1,5 +1,17 @@
 <?php
-require_once 'connections.php';
+session_start();
+require 'connections.php'; // Assumes $conn is a PDO instance
+
+// Ensure user is logged in
+if (!isset($_SESSION['user'])) {
+   // die("Unauthorized access.");
+    header("Location: login.php");
+}
+
+$user_id = $_SESSION['user']['user_id'];
+
+
+
 
 $invoiceData = null;
 $invoiceItems = [];
