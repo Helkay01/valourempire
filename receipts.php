0b6caccd2306d46ef996ffecf335b9668e74d80b
Helkay01 <83680190+Helkay01@users.noreply.github.com>
2025-08-31 23:30:35 +0100

    Update receipts.php

--- receipts.php
+++ receipts.php
@@ -325,8 +325,8 @@ foreach ($dets as $det) {
         <div>
           <label for="paymentMethod" class="block text-sm font-medium text-gray-700">Payment Method</label>
           <select name="paymentMethod" id="paymentMethod" class="mt-1 w-full border px-4 py-2 rounded focus:ring-2 focus:ring-blue-500">
-            <option>Cash</option>
-            <option>Bank Transfer</option>
+            <option value="Cash">Cash</option>
+            <option value="Bank">Bank Transfer</option>
           </select>
         </div>
       </div>
