617ddf0e794b510002c3b902e0ab7acc4531c0c5
Helkay01 <83680190+Helkay01@users.noreply.github.com>
2025-08-31 23:49:35 +0100

    Update create-invoice.php

--- create-invoice.php
+++ create-invoice.php
@@ -104,7 +104,7 @@ try {
 <html lang="en">
 <head>
   <meta charset="UTF-8"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  
   <title>Create Invoice</title>
   <script src="https://cdn.tailwindcss.com"></script>
 
