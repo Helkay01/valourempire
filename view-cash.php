46620fd372c8349af5edf9356612c12c03e395c5
Helkay01 <83680190+Helkay01@users.noreply.github.com>
2025-09-06 12:38:04 +0100

    Update view-cash.php

--- view-cash.php
+++ view-cash.php
@@ -108,7 +108,7 @@ if ($startDate && $endDate) {
                     <td class="px-4 py-2"><?= htmlspecialchars(date("d M Y", strtotime($txn['date']))) ?></td>
                     <td class="px-4 py-2"><?= htmlspecialchars($txn['note']) ?></td>
                     <td class="px-4 py-2">₦<?= $txn['amount'] ?></td>
-                    <td class="px-4 py-2">₦<?= $txn['type'] ?></td>
+                    <td class="px-4 py-2"><?= $txn['type'] ?></td>
                   </tr>
                 <?php endforeach; ?>
               <?php endif; ?>
