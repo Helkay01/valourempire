9ad9f197bb6b1c179fd593f904bad8ae95d28140
Helkay01 <83680190+Helkay01@users.noreply.github.com>
2025-09-15 23:40:46 +0100

    Update delete-expenses.php

--- delete-expenses.php
+++ delete-expenses.php
@@ -28,7 +28,7 @@ if(isset($_GET['id'])) {
 
     if(isset($_GET['pm']) && isset($_GET['amount'])) {
         $pm = $_GET['pm'];
-        $amount = $_GET['amount'];
+        $amount = (float)$_GET['amount'];
 
         if($pm == "cash" || $pm == "Cash") {
              $selCashBal = $pdo->prepare("SELECT * FROM cash_bal");
